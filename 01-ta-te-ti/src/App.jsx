f9771bbfd1b8973a67b287da2ccf7a7874a86191
ramirobarrasoto <rbarrasoto@gmail.com>
2024-01-14 18:59:56 -0300

    buttom to reset the game in every moment

--- 01-ta-te-ti/src/App.jsx
+++ 01-ta-te-ti/src/App.jsx
@@ -77,6 +77,7 @@ function App() {
   return (
     <main className='board'>
       <h1>Ta Te Ti</h1>
+      <button onClick={resetGame}>Reiniciar el juego</button>
       <section className='game'>
         {board.map((_, index) => {
           return (
